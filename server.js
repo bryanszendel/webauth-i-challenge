94cacc50f46f570de49eb07ad5ab69be61d8837e
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-16 14:28:25 -0600

    added index, server, and router files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const db = require('./data/db-config.js');
+
+const server = express();
+const usersRouter = require('./users/users-router.js')
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// routes
+server.use('/users', usersRouter)
+
+module.exports = server;
